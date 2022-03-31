d36aa0b6ffd765219a2fcb00d25f416c4ac88515
luyingli <luyingli@163.com>
2022-03-31 16:09:14 +0800

    这是dev分支第一次提交

--- src/main/java/cn/jy/git/controller/TestController.java
+++ src/main/java/cn/jy/git/controller/TestController.java
@@ -3,6 +3,7 @@ package cn.jy.git.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git dev");
     }
 
 }
